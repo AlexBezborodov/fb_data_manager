380822916a146480e386bed1558bfdfe5fb4ad8c
Alex BEzborodov <alexbezborodov23@gmail.com>
2023-01-08 00:02:11 +0200

    Feature: add groups  menu item

--- src/components/menu_items/groups/groups.jsx
+++ src/components/menu_items/groups/groups.jsx
@@ -1,5 +1,145 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+
+import { DeleteOutlined, ReloadOutlined } from "@ant-design/icons";
+import { Button, message, Typography } from "antd";
+import axios from "axios";
+
+import { Box } from "../../../global_styles/global_styles";
+import { CurrentUserContext } from "../../../providers/current_user";
+import { BASIC_DB_URL, CONFIG } from "../../../variables";
+import { BasicSearch } from "../../basic_components";
+import { CustomTable } from "../../basic_components/table";
+import { Container, Wrapper, ContentContainer } from "./styles";
 
 export const Groups = () => {
-  return <div>groups</div>;
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+  const columns = [
+    {
+      title: "List name",
+      dataIndex: "name",
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: "Spreadsheet link",
+      dataIndex: "slink",
+      Width: 150,
+      render: (text) => (
+        <a href={text} target="_blank" rel="noreferrer">
+          {text}
+        </a>
+      ),
+    },
+    {
+      title: "Send email",
+      dataIndex: "isSend",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      visible: true,
+      width: 90,
+      render: (index, item) => (
+        <Box style={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            type="primary"
+            shape="circle"
+            icon={<DeleteOutlined />}
+            size="small"
+            danger
+            onClick={() => removeItem(item.key)}
+          />
+        </Box>
+      ),
+    },
+  ];
+
+  const [tableData, setTableData] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const userId = localStorage.getItem("userId");
+
+  function transformData(listsData) {
+    if (listsData) {
+      setTableData(
+        listsData.map((item, i) => ({
+          key: item.id,
+          name: item.groupName,
+          slink: item.spreadsheetLink,
+          isSend: false,
+        }))
+      );
+    } else {
+      setTableData([]);
+    }
+  }
+
+  const removeItem = (id) => {
+    const updatedData = currentUser?.fbGroups.filter((item) => item.id !== id);
+    axios
+      .patch(
+        `${BASIC_DB_URL}/users/user${userId}.json`,
+        { ...currentUser, fbGroups: updatedData },
+        CONFIG
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          setCurrentUser(res.data);
+          message.success("Item removed!");
+        } else {
+          message.error("Something went wrong. Try again later");
+        }
+      });
+  };
+
+  const refresh = () => {
+    axios.get(`${BASIC_DB_URL}/users/user${userId}.json`).then((res) => {
+      if (res.status === 200) {
+        setCurrentUser(res.data);
+        message.success("Data updated");
+      } else {
+        message.error("Something went wrong. Try again later");
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (searchValue) {
+      const filteredData = currentUser?.fbGroups.filter((item) =>
+        item?.groupName?.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      transformData(filteredData);
+    } else {
+      transformData(currentUser?.fbGroups);
+    }
+  }, [currentUser?.fbGroups, searchValue]);
+
+  return (
+    <>
+      <Container>
+        <Wrapper>
+          <ContentContainer>
+            <Typography.Title variant={2}>Group list</Typography.Title>
+            <Box
+              style={{ display: "flex", margin: "10px auto", maxWidth: 800 }}
+            >
+              <BasicSearch value={searchValue} setValue={setSearchValue} />
+            </Box>
+            <CustomTable data={tableData} columns={columns} />
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<ReloadOutlined />}
+              size="medium"
+              style={{
+                position: "absolute",
+                top: 8,
+                right: 8,
+              }}
+              onClick={refresh}
+            />
+          </ContentContainer>
+        </Wrapper>
+      </Container>
+    </>
+  );
 };
---
+++ src/components/menu_items/groups/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 1rem;
+  margin: 1rem auto;
+  border-radius: 1rem;
+  border: 1px solid grey;
+`;
+export const ContentContainer = styled.div`
+  height: 780px;
+  background: lightgrey;
+  border-radius: 1rem;
+  margin: 1rem;
+  padding: 1rem;
+  overflow-y: auto;
+  position: relative;
+`;
+
+export const ActionsContainer = styled.div`
+  margin: 0 1rem;
+  display: flex;
+  justify-content: flex-end;
+`;
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  height: 89vh;
+`;
