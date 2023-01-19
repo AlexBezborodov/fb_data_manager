163b3a23907aa96cf7a02ea33483675a0b53b75e
Alex BEzborodov <alexbezborodov23@gmail.com>
2023-01-19 13:48:27 +0200

    clean: remove commented code

--- src/variables/variables.js
+++ src/variables/variables.js
@@ -1,11 +1,11 @@
 export const CLIENTID =
   "60242999873-2a8on5vdf5193u1ujcr0jnq6nomggl69.apps.googleusercontent.com";
 
-export const BASIC_DB_URL = "https://fbextension-default-rtdb.firebaseio.com";
-//"https://sheetdb.io/api/v1/1lhpldplc8hwi";
-//"https://sheet.best/api/sheets/3b47ce94-5574-4370-98ac-1a689199c20a";
+//export const BASIC_DB_URL = "https://fbextension-default-rtdb.firebaseio.com";
+export const BASIC_DB_URL =
+  "https://fbextension-e124d-default-rtdb.europe-west1.firebasedatabase.app";
 export const MAIL_REGEXP = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
-//AIzaSyB1XdCD1MmzTNYtx-Y9cbYvYXD8Oyzvwsg
+
 export const CONFIG = {
   headers: {
     "Content-type": "application/json; charset=UTF-8",
